57bc14919a8e68ee9aeb2ea688e2dc6f8c1ce20e
Xavier Crochet <xacrochet@gmail.com>
2014-02-15 12:27:32 +0100

    Updated CourseEntity's CRUD action (and views) (to match new nesting)

--- prog_mgmt_tool/app/controllers/course_entities_controller.rb
+++ prog_mgmt_tool/app/controllers/course_entities_controller.rb
@@ -1,7 +1,8 @@
 class CourseEntitiesController < ApplicationController
-  before_filter :course, :program
+  before_filter :course, :program, :catalog, :p_module
   
   def index
+    @course_entities = @course.course_entities
   end
 
   def create
@@ -14,12 +15,11 @@ class CourseEntitiesController < ApplicationController
     @course_entity.save
   	#@course_entity = @course.course_entities.create(params[:course_entity].permit(:year, :professor, :credits))
   	
-  	
-  	redirect_to program_course_path(@program, @course)
+  	redirect_to catalog_program_p_module_course_course_entity_path(@catalog, @program, @p_module, @course, @course_entity)
   end
 
   def show
-  	@course_entity = CourseEntity.find(params[:id])
+  	@course_entity = @course.course_entities.find(params[:id])
   end
 
   def new
@@ -32,23 +32,40 @@ class CourseEntitiesController < ApplicationController
 
   def update
     @course_entity = CourseEntity.find(params[:id])
-    
     if params.has_key?(:course_entity)
-      @course_entity.update(params[:course_entity].permit(:year, :professor, :credits))
-      redirect_to program_course_path(@program, @course)
+      params[:course_entity].permit(:year, :professor, :credits)
+      @course_entity.professor = params[:course_entity][:professor]
+      @course_entity.credits = params[:course_entity][:credits]
+      @course_entity.course_id = params[:course_id]
+      @course_entity.year = params[:date][:year]
+      @course_entity.save
+      redirect_to catalog_program_p_module_course_course_entity_path(@catalog, @program, @p_module, @course, @course_entity)
     else
       render 'edit'
     end
   end
 
   def destroy
-  	@course =  Course.find_by(params[:course_id])
   	@course_entity = @course.course_entities.find(params[:id])
   	@course_entity.destroy
-  	redirect_to program_course_path(@program, @course)
+  	redirect_to catalog_program_p_module_course_course_entities_path(@catalog, @program, @p_module, @course)
   end
 
   private
+
+    def course_entity_params
+
+      params.require(:course_entity).permit(:year, :professor, :credits)
+      
+    end
+
+    def catalog
+      @catalog = Catalog.find(params[:catalog_id])
+    end
+
+    def p_module
+      @p_module = PModule.find(params[:p_module_id])
+    end
 
     def course
       @course = Course.find(params[:course_id])
