8c440409d230e9531fb5548ddb6ad28c3f8190f2
Xavier Crochet <xacrochet@gmail.com>
2014-04-23 18:58:25 +0200

    Added p_module class & p_module tests

--- prog_mgmt_tool/lib/constraints_checker/entities/p_module.rb
+++ prog_mgmt_tool/lib/constraints_checker/entities/p_module.rb
@@ -1,39 +1,14 @@
-module constraints_checker
-  module entities
+require 'entity'
+module ConstraintsChecker
+  module Entities
     class PModule < Entity
-      def initialize(id, name, catalog)
-        super(id, name, catalog)
-      end
-
-      def add_course(course)
-        courses[course.id] = course
-      end
-
       def find_course(course_id)
-        p "searching course <"+course_id.to_s+">"
-        if courses[course_id].nil?
-          p "Course not found :-/"
+        @childrens.each do |children|
+          if children.class.name.eql? "ConstraintsChecker::Entities::Course" and children.id.eql? course_id.to_s
+            return children
+          end
         end
-        courses[course_id]
-      end
-
-      def count_credits
-        credits = 0
-        self.sub_modues.each do |m|
-          credits = credits + m.count_credits.to_i
-        end
-        self.courses.each do |c|
-          credits = credits + c.credits.to_i unless c.credits.eql? 'NONE'
-        end
-        credits
-      end
-
-      def check_min(value)
-        self.count_credits >= value
-      end
-
-      def check_max(value)
-        self.count_credits <= value
+        return nil
       end
     end
   end
