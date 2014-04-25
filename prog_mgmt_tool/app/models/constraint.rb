6f1dd57001f63b046960bb72596e16a70cdde183
Xavier Crochet <xacrochet@gmail.com>
2014-04-25 15:20:34 +0200

    Cleaned Catalog model

--- prog_mgmt_tool/app/models/constraint.rb
+++ prog_mgmt_tool/app/models/constraint.rb
@@ -41,26 +41,11 @@ class Constraint < ActiveRecord::Base
 		end
 	end
 
-	def is_binary_corequisite?
-		self.role.eql? 'IN' and self.constraint_type.name.eql? 'COREQUISITE' and self.constraint_set.constraint_set_type.name = 'BINARY'
-	end
-
 	def to_object(target_object)
 		if self.constraint_type.name.eql? "PREREQUISITE"
 			ConstraintsChecker::Constraints::Prerequisite.new(self.entity.id, target_object)
 		elsif self.constraint_type.name.eql? "COREQUISITE"
 			ConstraintsChecker::Constraints::Corequisite.new(self.entity.id, target_object)
 		end
-	end
-
-	def pairs
-		if self.role.eql? 'OUT'
-			self.constraint_set.constraints.in
-		elsif self.is_binary_corequisite?
-			self.constraint_set.constraints.out
-		else
-			Constraint.none
-		end
-	end
-
+	end	
 end
