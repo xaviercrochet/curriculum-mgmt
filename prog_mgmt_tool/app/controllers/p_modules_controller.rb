7e5f0ec4c1a5dbc0047e297f25bddd8a3535e7ac
Xavier Crochet <xacrochet@gmail.com>
2014-04-25 13:51:38 +0200

    Updated PModule controller to match new arch

--- prog_mgmt_tool/app/controllers/p_modules_controller.rb
+++ prog_mgmt_tool/app/controllers/p_modules_controller.rb
@@ -1,19 +1,19 @@
 class PModulesController < ApplicationController
 
   def index
-    @program = Program.find(params[:program_id])
-  	@p_modules = @program.p_modules
+    @catalog = Catalog.find(params[:catalog_id])
+  	@p_modules = @catalog.p_modules
 	end
 
   def show
   	@p_module = PModule.find(params[:id])
-    @program = @p_module.program
+    @catalog = @p_module.catalog
   end
 
   def destroy
-    @program = Program.find(params[:id])
-  	@p_module = @program.p_modules.find(params[:format])
+    @catalog = Catalog.find(params[:id])
+  	@p_module = @catalog.p_modules.find(params[:format])
   	@p_module.destroy
-  	redirect_to program_p_modules_path(@program)
+  	redirect_to catalog_p_modules_path(@program)
   end
 end
