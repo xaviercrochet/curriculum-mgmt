327eb1cd7dac42b1083ec808f1c8f8b12bb11707
Xavier Crochet <xacrochet@gmail.com>
2014-04-30 18:21:12 +0200

    Fixed migration bug

--- db/migrate/20140429010049_refactor_p_module.rb
+++ db/migrate/20140429010049_refactor_p_module.rb
@@ -1,6 +1,5 @@
 class RefactorPModule < ActiveRecord::Migration
   def change
-    add_column :p_modules, :parent_id, :integer
     add_index :p_modules, :parent_id
     remove_column :p_modules, :program_id
   end
