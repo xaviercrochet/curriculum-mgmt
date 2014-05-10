087c426417c0fa3fa94b4628baeab221b0cef80b
Xavier Crochet <xacrochet@gmail.com>
2014-05-10 16:16:56 +0200

    Added french support

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module CurriculumMgmt
   class Application < Rails::Application
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
     config.serve_static_assets = true
+    config.i18n.default_locale = :fr
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
