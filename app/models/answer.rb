58ad2a368c9b61a28fd35be3958d137587050863
Xavier Crochet <xacrochet@gmail.com>
2014-05-15 11:47:39 +0200

    Added discussion link in studentProgram index

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,7 @@ class Answer < ActiveRecord::Base
   belongs_to :comment
   validates :content, presence: true
   belongs_to :user
+
+  scope :unread, -> {where("read = ?", false)}
+  scope :read, -> {where("read= ?", true)}
 end
