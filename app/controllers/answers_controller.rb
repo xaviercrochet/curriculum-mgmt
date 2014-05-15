b98924a7399f106e6786a417ac2accc27429d76f
Xavier Crochet <xacrochet@gmail.com>
2014-05-15 11:59:48 +0200

    Added some scope to answer model

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,11 +1,11 @@
 class AnswersController < ApplicationController
+  before_action :authenticate_user!
   def create
     @answer = Answer.new
     @answer.content = params[:answer][:content]
     @answer.user = current_user
     @comment = Comment.find(params[:comment_id])
-    @comment.read = true
-    @comment.save
+    @comment.tag_as_read(current_user)
     @answer.comment = @comment
     @answer.save
     redirect_to comments_path
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,4 +5,6 @@ class Answer < ActiveRecord::Base
 
   scope :unread, -> {where("read = ?", false)}
   scope :read, -> {where("read= ?", true)}
+  scope :from, -> (user) {where("user_id = ?", user.id)}
+  scope :not_from, -> (user) {where("user_id != ?", user.id)}
 end
