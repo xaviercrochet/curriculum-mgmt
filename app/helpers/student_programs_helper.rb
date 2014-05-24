62e099768867a4cf2d84ddfc2b8a70e3255043c2
Xavier Crochet <xacrochet@gmail.com>
2014-05-24 14:13:29 +0200

    Added helper method to dispatch to correct constraint_view

--- app/helpers/student_programs_helper.rb
+++ app/helpers/student_programs_helper.rb
@@ -1,2 +1,25 @@
 module StudentProgramsHelper
+
+  def render_constraints(exceptions, type)
+    case type
+    when "pre"
+      render partial: 'student_programs/check/prerequisites', locals: {exceptions: exceptions}
+    when "co"
+      render partial:'student_programs/check/corequisites', locals: {exceptions: exceptions}
+    when "mandatory"
+      render partial:'student_programs/check/mandatories', locals: {exceptions: exceptions}
+    when "or_co"
+      render partial:'student_programs/check/or_corequisites', locals: {exceptions: exceptions}
+    when "or_pre"
+      render partial:'student_programs/check/or_prerequisites', locals: {exceptions: exceptions}
+    when "xor_co"
+      render partial:'student_programs/check/xor_corequisites', locals: {exceptions: exceptions}
+    when "xor_pre"
+      render partial:'student_programs/check/xor_prerequisites', locals: {exceptions: exceptions}
+    when "min"
+      render partial:'student_programs/check/min', locals: {exceptions: exceptions}
+    when "max"
+      render partial:'student_programs/check/max', locals: {exceptions: exceptions}
+    end
+  end
 end
