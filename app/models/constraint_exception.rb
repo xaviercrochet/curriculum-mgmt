9270eaa4d8cd0fe74e5747c6a2f85ce66c480ac3
Xavier Crochet <xacrochet@gmail.com>
2014-05-24 13:08:58 +0200

    Added partial for check view

--- app/models/constraint_exception.rb
+++ app/models/constraint_exception.rb
@@ -3,13 +3,14 @@ class ConstraintException < ActiveRecord::Base
   belongs_to :entity, polymorphic: true
 
   scope :prerequisite, ->{where(constraint_type: "Prerequisite")}
-  scope :corequisites, -> {where(constraint_type: "Corequisite")}
+  scope :corequisite, -> {where(constraint_type: "Corequisite")}
   scope :or_prerequisite, -> {where(constraint_type: "OrPrerequisite")}
   scope :or_corequisite, -> {where(constraint_type: "OrCorequisite")}
   scope :xor_prerequisite, -> {where(constraint_type: "XorPrerequisite")}
   scope :xor_corequisite, -> {where(constraint_type: "XorCorequisite")}
   scope :min, -> {where(constraint_type: "Min")}
   scope :max, ->{where(constraint_type: "Max")}
+  scope :mandatory, -> {where(constraint_type: "Mandatory")}
 
 
 
