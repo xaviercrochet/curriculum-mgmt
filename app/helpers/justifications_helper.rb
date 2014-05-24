71a6b57e7484a689fe403c60136dfd310056340e
Xavier Crochet <xacrochet@gmail.com>
2014-05-24 19:55:34 +0200

    Justification view now show mandatory courses

--- app/helpers/justifications_helper.rb
+++ app/helpers/justifications_helper.rb
@@ -1,24 +1,24 @@
 module JustificationsHelper
-  def render_constraint(exception)
+  def render_constraint(exception, student_program)
     case exception.constraint_type
     when "Prerequisite"
-      render partial: 'prerequisite', locals: {exception: exception}
+      render partial: 'prerequisite', locals: {exception: exception, student_program: student_program}
     when "Corequisite"
-      render partial:'corequisite', locals: {exception: exception}
+      render partial:'corequisite', locals: {exception: exception, student_program: student_program}
     when "Mandatory"
-      render partial:'mandatorie', locals: {exception: exception}
+      render partial:'mandatory', locals: {exception: exception, student_program: student_program}
     when "OrCorequisite"
-      render partial:'or_corequisite', locals: {exception: exception}
+      render partial:'or_corequisite', locals: {exception: exception, student_program: student_program}
     when "OrPrerequisite"
-      render partial:'or_prerequisite', locals: {exception: exception}
+      render partial:'or_prerequisite', locals: {exception: exception, student_program: student_program}
     when "XorCorequisite"
-      render partial:'xor_corequisite', locals: {exception: exception}
+      render partial:'xor_corequisite', locals: {exception: exception, student_program: student_program}
     when "XorPrerequisite"
-      render partial:'xor_prerequisite', locals: {exception: exception}
+      render partial:'xor_prerequisite', locals: {exception: exception, student_program: student_program}
     when "Min"
-      render partial:'min', locals: {exception: exception}
+      render partial:'min', locals: {exception: exception, student_program: student_program}
     when "Max"
-      render partial:'max', locals: {exception: exception}
+      render partial:'max', locals: {exception: exception, student_program: student_program}
     end
   end
 end
