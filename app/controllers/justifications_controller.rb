2aeb81d812f1a6fd5759bdacd07f838f3ad7ac00
Xavier Crochet <xacrochet@gmail.com>
2014-05-27 12:59:59 +0200

    Fixed some N+1 queries

--- app/controllers/justifications_controller.rb
+++ app/controllers/justifications_controller.rb
@@ -20,12 +20,12 @@ class JustificationsController < ApplicationController
   end
 
   def show
-    @justification = Justification.includes(constraint_exceptions: :entity).find(params[:id])
+    @justification = Justification.includes(answers: :user, constraint_exceptions: :entity, student_program: [years: [:first_semester, :second_semester]]).find(params[:id])
     @answer = Answer.new
   end
 
   def index
-    @justifications = Justification.all
+    @justifications = Justification.includes(answers: :user, constraint_exceptions: :entity, student_program: [years: [:first_semester, :second_semester]]).all
     @answer = Answer.new
   end
 
